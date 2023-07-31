4e69b0be3dda4799105baa8d6511d7372cf50ded
Sangeeta Rani <110674541+Sangeeta-Rani14@users.noreply.github.com>
2023-07-31 15:42:39 +0530

    lesson 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Counter from './feature/counter/Counter';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Counter/>
+    </div>
+  );
+}
+
+export default App;
